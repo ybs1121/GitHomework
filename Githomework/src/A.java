c270be61768451da7768862c98b6f93292481ade
YBS <YBS@DESKTOP-GJ7RMHT>
2021-06-11 14:43:25 +0900

    Change A,C

--- Githomework/src/A.java
+++ Githomework/src/A.java
@@ -3,6 +3,7 @@ public class A {
 	public static void main(String args[]) {
 		System.out.println("A");
 		System.out.println("A2");
+		System.out.println("A3");
 		
 	}
 
--- Githomework/src/C.java
+++ Githomework/src/C.java
@@ -2,6 +2,7 @@
 public class C {
 	public void name(){
 		System.out.println("C");
+		System.out.println("C2");
 	}
 
 
